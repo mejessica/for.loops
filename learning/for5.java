81ca3e2ee10473b50b3e8f305b5349df46e814fb
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-03-20 20:03:33 -0300

    Update and rename for5 to learning/for5.java

--- learning/for5.java
+++ learning/for5.java
@@ -1,12 +1,9 @@
-package For;
 
-public class for5
-{
-    public static void main(String[] args)
-    {
+
+public class for5{
+    public static void main(String[] args){
         int contador;
-        for(contador = 5; contador <=50 ; contador+=5)
-        {
+        for(contador = 5; contador <=50 ; contador+=5){
             System.out.println( contador );
         }
         System.out.println("Obrigado por utilizar o sistema!");
